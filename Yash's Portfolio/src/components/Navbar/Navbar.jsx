c7d03fd2fecb0686fbe3d02e919af9cdb0966e0e
Aakash-Sondagar <aakashsondar@gmail.com>
2023-03-20 02:35:16 +0530

    Configuring Tailwind CSS

---
+++ Yash's Portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+  return (
+    <div class="">
+      <ul class="flex">
+        <li class="mr-12">
+          <a class="text-black-100 hover:text-gray-900 hover:underline" href="#">
+            About
+          </a>
+        </li>
+        <li class="mr-12">
+          <a class="text-black-100 hover:text-gray-900 hover:underline" href="#">
+            Project
+          </a>
+        </li>
+        <li class="mr-12">
+          <a class="text-black-100 hover:text-gray-900 hover:underline" href="#">
+            Experience
+          </a>
+        </li>
+        <li class="mr-12">
+          <div>Icons</div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
